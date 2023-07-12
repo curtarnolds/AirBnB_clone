f16ecfe7e6a1043d452924101bdd364873945498
curtarnolds <afayedormawuli@gmail.com>
2023-07-12 15:46:49 +0000

    Initial implementation of BaseModel. * Added functionality to serialize and deserialize the BaseModel instances to and from json * FileStorage implements writing and reading json from a file in the current directory

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,45 @@
 #!/usr/bin/python3
+"""A base_model module to define parent class of all other classes.
+"""
+import uuid
+import datetime
+from models import storage
 
+
+class BaseModel:
+    """Defines common attributes/methods of other classes."""
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize a BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ('created_at', 'updated_at'):
+                    value = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+            storage.save()
+
+    def __str__(self) -> str:
+        """Return the string representation of the BaseModel instance."""
+        return f'[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>'
+
+    def save(self) -> None:
+        """Log the time BaseModel instance was updated."""
+        self.updated_at = datetime.datetime.now()
+        storage.new(self)
+        storage.save()
+
+    def to_dict(self) -> dict:
+        """Return a dictionary representation of the BaseModel instance."""
+        tmp_dict = {'__class__': self.__class__.__name__}
+        for k, v in self.__dict__.items():
+            if k in ('created_at', 'updated_at'):
+                v = v.isoformat()
+            tmp_dict[k] = v
+        return tmp_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Contains the FileStorage class definition."""
+import json
+
+
+class FileStorage:
+    """Defines a FileStorage class."""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def __init__(self) -> None:
+        pass
+
+    def all(self) -> dict:
+        """Returns dictionary of all objects."""
+        return __class__.__objects
+
+    def new(self, obj) -> None:
+        """Sets in `__objects` the `obj` with key `<obj class name>.id`"""
+        __class__.__objects[
+            f'{obj.__class__.__name__}.{obj.id}'] = obj.to_dict()
+
+    def save(self) -> None:
+        """Serialize `__objects` to JSON file specified in `__file_path`."""
+        with open(__class__.__file_path, 'w') as json_file:
+            json.dump(__class__.__objects, json_file)
+
+    def reload(self) -> None:
+        """Deserialize JSON file specified in `__file_path` to `__objects`"""
+        try:
+            with open(__class__.__file_path) as json_file:
+                __class__.__objects = json.load(json_file)
+        except FileNotFoundError:
+            pass
