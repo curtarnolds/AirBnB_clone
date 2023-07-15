4196421f8bb9fca40f5c39537a8f12b6f54eff74
curtarnolds <afayedormawuli@gmail.com>
2023-07-15 22:26:44 +0000

    Added method level docstrings to User test suite

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,12 +10,14 @@ class TestUser(unittest.TestCase):
     """Test cases against the User class"""
 
     def test_attrs_are_class_attrs(self):
+        """Check if attributes are class attributes."""
         u = User() # noqa
         # test that it is a class attribute
         self.assertTrue(hasattr(User, "first_name")
                         and hasattr(User, "last_name"))
 
     def test_class_attrs(self):
+        """Check if the class attributes are assigned their default values."""
         u = User()
         self.assertIs(type(u.first_name), str)
         self.assertIs(type(u.last_name), str)
@@ -23,5 +25,6 @@ class TestUser(unittest.TestCase):
         self.assertTrue(u.last_name == "")
 
     def test_user_is_a_subclass_of_basemodel(self):
+        """Check if User is a subclass of BaseModel."""
         u = User()
         self.assertTrue(issubclass(type(u), BaseModel))
